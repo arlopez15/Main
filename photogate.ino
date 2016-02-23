7cf23ab2e86e24dbee01843dca6614840d44bf25
arlopez15 <adamrlopez15@gmail.com>
2016-02-23 16:52:45 -0600

    Delete photogate.ino

--- photogate.ino
+++
@@ -1,9 +0,0 @@
-
-//Take sum of 32 readings (interpret as 15-bit average instead of 10-bit)
-//(Why 32? maximimum readings that fit inside signed 16-bit, and still fast)
-int photogateAverage() {
-  int average = 0;
-  for(int count = 0; count < 32; count++)
-    average += analogRead(PHOTOGATE_PIN);
-  return average;
-}
