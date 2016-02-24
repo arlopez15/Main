a1b72dfda604bb0b953fa45311a04e18b417fd30
arlopez15 <adamrlopez15@gmail.com>
2016-02-23 21:02:13 -0600

    Create infrared.ino

---
+++ main/infrared.ino
@@ -0,0 +1,8 @@
+
+//Based on sketch by Noah Stahl (2011) for Sharp GP2Y0A02YK0F
+float read_ir_long_range_cm(){
+	float sensorValue = analogRead(sensorIR);
+	//inches = 4192.936 * pow(sensorValue,-0.935) - 3.937;
+	float cm = 10650.08 * pow(sensorValue,-0.935) - 10;
+	return cm;
+}
