0fff9cde68d1542a429feb2e9ef7519801072899
arlopez15 <adamrlopez15@gmail.com>
2016-02-27 10:14:38 -0600

    Update infrared.ino

--- main/infrared.ino
+++ main/infrared.ino
@@ -1,4 +1,12 @@
+//Based on sketch by Noah Stahl (2011) for Sharp GP2Y0A02YK0F
+float read_ir_long_range_cm(int sensor_pin){
+	float sensorValue = analogRead(sensor_pin);
+	//inches = 4192.936 * pow(sensorValue,-0.935) - 3.937;
+	float cm = 10650.08 * pow(sensorValue,-0.935) - 10;
+	return cm;
+}
 
+/*
 //Based on sketch by Noah Stahl (2011) for Sharp GP2Y0A02YK0F
 float read_ir_long_range_cm(){
 	float sensorValue = analogRead(sensorIR);
@@ -6,3 +14,4 @@ float read_ir_long_range_cm(){
 	float cm = 10650.08 * pow(sensorValue,-0.935) - 10;
 	return cm;
 }
+*/
